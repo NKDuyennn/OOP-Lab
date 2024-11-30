5333a7734deb620e296e514d2f59bafc2581907a
Nguyễn Kiều Duyên <kieuduyennn2310@gmail.com>
2024-11-30 12:14:45 +0700

    Update the Store to work with Media

--- AimsProject/src/hust/soict/hedspi/test/store/StoreTest.java
+++ AimsProject/src/hust/soict/hedspi/test/store/StoreTest.java
@@ -9,24 +9,24 @@ public class StoreTest {
 		// Create new dvd objects and add them to the store
 		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
 				"Animation", "Roger Allers", 87, 19.95f);
-		store.addDVD(dvd1);
+		store.addMedia(dvd1);
 		
 		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars",
 				"Science Fiction", "George Lucas", 87, 24.95f);
-		store.addDVD(dvd2);
+		store.addMedia(dvd2);
 		
 		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", 
 				"Animation", 18.99f);
-		store.addDVD(dvd3);
+		store.addMedia(dvd3);
 		
 		store.print();
 		
-		store.addDVD(dvd3);
-		store.removeDVD(dvd3);
+		store.addMedia(dvd3);
+		store.removeMedia(dvd3);
 		
 		store.print();
 		
-		store.addDVD(dvd3);
+		store.addMedia(dvd3);
 		
 		store.print();
 	}
