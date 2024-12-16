2ef7ec9913a76a780db7b4e82ad057651486d07e
Nguyễn Kiều Duyên <kieuduyennn2310@gmail.com>
2024-12-17 00:02:02 +0700

    add fx:id in cart.fxml

---
+++ AimsProject/src/hust/soict/hedspi/aims/screen/controller/CartScreenController.java
@@ -0,0 +1,20 @@
+package hust.soict.hedspi.aims.screen.controller;
+
+import hust.soict.hedspi.aims.cart.Cart;
+import hust.soict.hedspi.aims.media.Media;
+import hust.soict.hedspi.aims.media.Playable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.transformation.FilteredList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class CartScreenController {
+
+	public CartScreenController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
